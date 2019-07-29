98237a717c849d9c8cbb0b2fbd8879099a4ee0e2
Arcadius-Mejo <53377307+Arcadius-Mejo@users.noreply.github.com>
2019-07-29 15:40:04 +0530

    Add files via upload

---
+++ guvi2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    long int n;
+    scanf("%ld",&n);
+    if((n%2)==0 && n>=0)
+    printf("Even");
+    else if(n<0)
+    printf("invalid");
+    else
+    printf("Odd");
+    return 0;
+}
